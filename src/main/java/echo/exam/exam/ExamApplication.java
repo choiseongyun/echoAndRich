decdee00c75254cfe8bde744fd5bb178a2dbe2f4
choiseongyun <5songyon5@naver.com>
2023-06-04 22:22:18 +0900

    echoAndRich AWS 반영으로 인한 소스 수정

--- src/main/java/echo/exam/exam/ExamApplication.java
+++ src/main/java/echo/exam/exam/ExamApplication.java
@@ -2,11 +2,19 @@ package echo.exam.exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ExamApplication {
+public class ExamApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ExamApplication.class);
+	}
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ExamApplication.class, args);
 	}
 
